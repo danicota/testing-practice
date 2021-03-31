570de61c03cc7d79725244006374b62cc3384c90
danicota <danidoesweb@gmail.com>
2021-03-31 00:48:11 -0400

    product succesful cases

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(a, b) {
+return a * b
 }
 
 // ==================================================
@@ -20,22 +20,26 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
-
+  var result = product(0, 2);
+  if (result !== 0) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = product(0, 2, 1);
+  if (result !== 0) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(0, null);
+  if (result !== 0) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = product(null, null);
+  if (result !== 0) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
