07af301cfc02dd636264ce3fca33f58f2c7f7628
danicota <danidoesweb@gmail.com>
2021-03-31 00:58:44 -0400

    quotient cases success

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(a, b) {
+return a / b
 }
 
 // ==================================================
@@ -20,22 +20,26 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(2, 0);
+  if (result == NaN) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = quotient(2, 2, 0);
+  if (result !== 1) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = quotient(2, null);
+  if (result == 0) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = quotient(null, null);
+  if (result == 0) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
